c2586b8d1d8a184e4a8823ca2234e9aa30589be8
Julian Infanger <j.infanger@4teamwork.ch>
2009-08-25 08:23:53 +0000

    created ftw.securefiledownload with paster

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1-dev'
+
+setup(name='ftw.securefiledownload',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Julian Infanger',
+      author_email='info@4teamwork.ch',
+      url='https://svn.4teamwork.ch/repos/ftw/ftw.securefiledownload/trunk/',
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
