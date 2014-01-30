e74c9087814580f4c41a639a3c8816213909e843
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-01-30 16:24:40 +0100

    Package clean up.

---
+++ README.rst
@@ -0,0 +1,36 @@
+Introduction
+============
+
+``ftw.downloadtoken`` allows you to grant temporary access to a specific
+download-able content.
+
+You can send a email containing a url with token to one or multiple recipients.
+
+
+Usage
+=====
+
+To be defined...
+
+
+Compatibility
+-------------
+
+Runs with `Plone <http://www.plone.org/>`_ `4.1`, `4.2` or `4.3`.
+
+
+Links
+=====
+
+- Main github project repository: https://github.com/4teamwork/ftw.downloadtoken
+- Issue tracker: https://github.com/4teamwork/ftw.downloadtoken/issues
+- Package on pypi: http://pypi.python.org/pypi/ftw.downloadtoken
+- Continuous integration: https://jenkins.4teamwork.ch/search?q=ftw.downloadtoken
+
+
+Copyright
+=========
+
+This package is copyright by `4teamwork <http://www.4teamwork.ch/>`_.
+
+``ftw.downloadtoken`` is licensed under GNU General Public License, version 2.
--- setup.py
+++ setup.py
@@ -1,24 +1,33 @@
 from setuptools import setup, find_packages
 import os
 
-version = open("ftw/downloadtoken/version.txt").read().strip()
+version = '1.2.0-dev0'
 maintainer = "Julian Infanger"
 
 setup(name='ftw.downloadtoken',
       version=version,
-      description="maintainer: %s"%maintainer,
-      long_description=open("README.txt").read() + "\n" +
+      description='Grants temporary access to a specific download-able '
+                  'content.',
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Framework :: Plone :: 4.3',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
+
       keywords='',
-      author='%s, 4teamwork GmbH'%maintainer,
+      author='4teamwork AG',
       author_email='mailto:info@4teamwork.ch',
-      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.downloadtoken/',
+      url='https://github.com/4teamwork/ftw.downloadtoken',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
@@ -26,9 +35,10 @@ setup(name='ftw.downloadtoken',
       install_requires=[
           'setuptools',
           'ftw.sendmail',
-          # -*- Extra requirements: -*-
       ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
